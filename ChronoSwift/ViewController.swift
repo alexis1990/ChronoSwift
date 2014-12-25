d953f675295f15ac61c7f6985d80758601dd54a0
Alexandre Monjol <alexandre.monjol@gmail.com>
2014-12-25 15:55:28 +0100

    Touch reconize

--- ChronoSwift/ViewController.swift
+++ ChronoSwift/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var methodStatus: UILabel!
+    
+    @IBOutlet weak var touchStatus: UILabel!
 
+    @IBOutlet weak var tapStatus: UILabel!
+
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        let touchCount = touches.count
+        let touch = touches.anyObject() as UITouch
+        let tapCount = touch.tapCount
+        
+        methodStatus.text = "touchesBegan"
+        touchStatus.text = "\(touchCount) touches"
+        tapStatus.text = "\(tapCount) taps"
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        let touchCount = touches.count
+        let touch = touches.anyObject() as UITouch
+        let tapCount = touch.tapCount
+        
+        methodStatus.text = "touchesMoved"
+        touchStatus.text = "\(touchCount) touches"
+        tapStatus.text = "\(tapCount) taps"
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        let touchCount = touches.count
+        let touch = touches.anyObject() as UITouch
+        let tapCount = touch.tapCount
+        
+        methodStatus.text = "touchesEnded"
+        touchStatus.text = "\(touchCount) touches"
+        tapStatus.text = "\(tapCount) taps"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
